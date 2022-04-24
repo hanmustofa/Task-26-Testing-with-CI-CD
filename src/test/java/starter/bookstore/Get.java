c6ae2e59424eb3790a61d9688e0a1cdf18d409ed
Hanmustofa <32809284+hanmustofa@users.noreply.github.com>
2022-04-24 21:48:57 +0700

    add file

---
+++ src/test/java/starter/bookstore/Get.java
@@ -0,0 +1,24 @@
+package starter.bookstore;
+
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+
+public class Get {
+    protected static String url = "https://demoqa.com/BookStore/";
+
+    @Step("I set GET api endpoints")
+    public String setApiEndpoints(){
+        return url + "v1/books";
+    }
+
+    @Step("I sent GET HTTP request")
+    public void sendGetHttpRequest(){
+        SerenityRest.given().get(setApiEndpoints());
+    }
+    @Step("I receive valid HTTP response code 200")
+    public void validateHttpResponseCode200(){
+        restAssuredThat(response -> response.statusCode(200));
+    }
+}
